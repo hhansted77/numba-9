4a3b448dd125b1f40b4b487f288480502882351e
Hannah Hansted <hmh22@fsu.edu>
2024-10-27 19:59:18 -0400

    worst game of my life (evil ghost spins me)

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ let sparkle;
 let pscore = 0;
 let tscore = 0;
 
+let rotator;
+
 function setup() {
 	new Canvas(1000, 600);
 	displayMode('centered');
@@ -16,22 +18,27 @@ function setup() {
 	sparkle.diameter = 10;
 	sparkle.x = () => random(0, canvas.w);
 	sparkle.y = () => random(0, canvas.h);
-	sparkle.amount = 40;
+	sparkle.amount = 100;
 	sparkle.color = 'white';
 	sparkle.stroke = 'gold';
 
 	floor = new Sprite(500, 598, 1000, 5, 's');
 	floor.color = 'skyblue';
+	floor.stroke = 'skyblue';
 	wallL = new Sprite(0, 300, 6, 600, 's');
 	wallL.color = 'skyblue';
+	wallL.stroke = 'skyblue';
 	wallR = new Sprite(1000, 300, 6, 600, 's');
+	wallR.color = 'skyblue';
 	wallR.color = 'skyblue';
 	ceiling = new Sprite(500, 2, 1000, 5, 's');
 	ceiling.color = 'skyblue';
+	ceiling.stroke = 'skyblue';
 
 	spikes = new Turtle(30);
 	randomSequence();
 	spikes.overlap(sparkle, collector);
+	//spikes.overlap(rotator);
 
 	player = new Sprite();
 	player.diameter = 50;
@@ -40,14 +47,24 @@ function setup() {
 	player.color = 'pink';
 
 	player.overlaps(sparkle, collect);
+	//player.overlaps(rotator);
+
+	rotator = new Sprite(200, 157, 80, 10,);
+	rotator.color = 'skyblue';
+	rotator.stroke = 'skyblue';
+	rotator.vel.x = 5;
+	rotator.vel.y = 7;
+	rotator.rotationSpeed = 6;
+	rotator.bounciness = 5;
+
 	
 
 
 	async function randomSequence() {
 		let x = random(0, width);
 		let y = random(0, height);
-		await spikes.rotateTo(x, y, 9);
-		await spikes.moveTo(x, y, 12);
+		await spikes.rotateTo(x, y, 20);
+		await spikes.moveTo(x, y, 27);
 		randomSequence();
 	}
 
@@ -79,7 +96,7 @@ function draw() {
 	
 	//sparkle.attractTo(ball, 5);
 
-	player.moveTowards(mouse, .04);
+	player.moveTowards(mouse, .02);
 
 
 }
